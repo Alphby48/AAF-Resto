7633bb2ca2fe08d98879c2e2320adba6d34d63a0
Alphby48 <hazerdmobile@gmail.com>
2024-01-03 00:58:13 +0700

    perubahan ketiga

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -1,10 +1,38 @@
 class AppBar extends HTMLElement {
+  constructor() {
+    super();
+    this.shadowDOM = this.attachShadow({ mode: "open" });
+  }
+
   connectedCallback() {
     this.render();
   }
 
   render() {
-    this.innerHTML = `
+    this.shadowDOM.innerHTML = `
+    <style>
+    * {
+    margin: 0;
+    padding: 1rem 0;
+    box-sizing: border-box;
+    }
+    :host {
+      display: flex;
+      align-items: center;
+      gap: 40px;
+      justify-content: center;
+      background-color: aqua;
+      
+    }
+    img{
+      width: 100px;
+    }
+    h2{
+      font-family: "Poppins-Bold";
+      font-size: 2.5rem;
+    }
+    </style>
+    
     <img src="asset/img/resto aaf.png" alt="" />
     <h2>AAF Resto</h2>
         `;
--- src/script/component/search-bar.js
+++ src/script/component/search-bar.js
@@ -1,4 +1,9 @@
 class SearchBar extends HTMLElement {
+  constructor() {
+    super();
+    this.shadowDOM = this.attachShadow({ mode: "open" });
+  }
+
   connectedCallBack() {
     this.render;
   }
@@ -9,11 +14,40 @@ class SearchBar extends HTMLElement {
   }
 
   get value() {
-    return this.querySelector("#kolomCari").value;
+    return this.shadowDOM.querySelector("#kolomCari").value;
   }
 
   render() {
-    this.innerHTML = `
+    this.shadowDOM.innerHTML = `
+      <style>
+      :host {
+        width: 80%;
+      }
+      .pencarian {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        gap: 30px;
+      }
+      .pencarian input {
+        width: 60%;
+        height: 40px;
+        border-radius: 5px;
+        padding: 5px;
+        font-family: "Poppins";
+      }
+      .pencarian button {
+        height: 40px;
+        width: 80px;
+        border-radius: 5px;
+        background-color: #00ffdd;
+        font-size: 16px;
+        font-family: "Poppins";
+      }
+      .pencarian button:active {
+        background-color: rgb(94, 255, 0);
+      }
+      </style>
         <div class="pencarian" id="pencarian">
         <input
           type="search"
@@ -24,10 +58,9 @@ class SearchBar extends HTMLElement {
         <button id="tombolCari" type="submit">Cari</button>
       </div>
         `;
-    this.querySelector("#tombolCari").addEventListener(
-      "click",
-      this._clickEvent
-    );
+    this.shadowDOM
+      .querySelector("#tombolCari")
+      .addEventListener("click", this._clickEvent);
   }
 }
 
